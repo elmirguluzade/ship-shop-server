921e9001f765ff12f869301b35fef9823c6e3c3d
Elmir Guluzade <elmirguluzade@Elmirs-MacBook-Air.local>
2024-06-05 00:19:28 +0900

    url changed

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const userRouter = require('../routes/userRouter');
 const productRouter = require('../routes/productRouter');
 // require("dotenv").config({ path: "./config.env" });
 
-app.use(cors({credentials: true, origin: "https://shipshop-client.vercel.app"}))
+app.use(cors({credentials: true, origin: "https://shipshops.vercel.app"}))
 app.use(express.json())
 app.use(cookieParser())
 
